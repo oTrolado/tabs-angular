062980690295f5bc1e8b4f4e2f2ad0d0de9315f0
oTrolado <gabriel.costa41@fatec.sp.gov.br>
2019-05-17 09:53:37 -0300

    arrows

--- src/app/components/tabs/tabs.component.ts
+++ src/app/components/tabs/tabs.component.ts
@@ -1,12 +1,12 @@
-import { Component, OnInit } from '@angular/core';
-import { element } from '@angular/core/src/render3';
+import { Component, OnInit, AfterViewInit, ElementRef, ViewChild } from '@angular/core';
+import { query } from '@angular/core/src/render3';
 
 @Component({
   selector: 'trolado-tabs',
   templateUrl: './tabs.component.html',
   styleUrls: ['./tabs.component.scss']
 })
-export class TabsComponent implements OnInit {
+export class TabsComponent implements OnInit, AfterViewInit {
 
   labels:any = [];
   contents:any;
@@ -15,6 +15,10 @@ export class TabsComponent implements OnInit {
   wraper:any;
   animable: boolean = true;
   indicator:any;
+  arrows:boolean = true;
+
+  @ViewChild("header") header: ElementRef;
+  @ViewChild("headerContainer") headerContainer: ElementRef;
 
   constructor() { }
 
@@ -35,10 +39,37 @@ export class TabsComponent implements OnInit {
     this.wraper = document.getElementsByClassName('trolado-tabs-wraper')[0];
     
     this.indicator = document.getElementsByClassName('trolado-tabs-indicator')[0];
+    
+
     this.indicator.style.width = (100/this.labels.length)+'%';
-    setTimeout(()=> document.getElementsByClassName('trolado-tab-label-item')[0].classList.add('active-label'), 500);
 
   }
+
+
+  ngAfterViewInit(){
+    
+    document.getElementsByClassName('trolado-tab-label-item')[0].classList.add('active-label');
+
+
+    this.header.nativeElement.style.minWidth = this.labels.length * 75 + 'px';
+    
+  }
+
+  onResize(event){
+    let container = this.headerContainer.nativeElement.clientWidth;
+    let element = this.header.nativeElement.clientWidth;
+    console.log(container+ ' ' +element);
+    if(container < element){
+      this.arrows = true;
+      return true;
+    } 
+    
+    this.arrows = false;
+    this.header.nativeElement.style.left = '0';
+    return false;
+
+  }
+
 
   exibir(item){
 
@@ -74,20 +105,46 @@ export class TabsComponent implements OnInit {
    
   }
 
+
+
+
   ripple(event, element){
     let elements = document.getElementsByClassName('trolado-tab-label-item');
 
     let div = document.createElement('div');
     div.classList.add('ripple');
+
     elements[element].appendChild(div);
 
     div.style.top = (event.layerY)+"px";
     div.style.left = (event.layerX)+"px";
-    div.style.transform = 'scale('+(event.target.clientWidth*2.2)/12+')';
-    console.log(div);
-    console.log(event);
+
+    if(event.target.clientWidth >= event.target.clientHeight){
+      
+      div.style.transform = 'scale('+(event.target.clientWidth*2.2)/12+')';
+    
+    } else {
+      console.log(event);
+      div.style.transform = 'scale('+(event.target.clientHeight*2.8)/12+')';
+
+    }
+
+
     setTimeout(()=>div.style.backgroundColor = 'transparent', 400);
     setTimeout(()=>div.remove(), 800);
+  }
+
+  navigate(direction){
+    let position = parseInt(this.header.nativeElement.style.left + 1);
+
+    if(direction == 'right'){
+
+      this.header.nativeElement.style.left = position - 60+'px';
+
+    } else {
+
+      this.header.nativeElement.style.left = position + 60+'px';
+    }
   }
 }
  
