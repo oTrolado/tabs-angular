f415bc8ed9821db179d5dcc6978af153964a9087
oTrolado <gabriel.costa41@fatec.sp.gov.br>
2019-05-24 10:23:40 -0300

    ripple v1

--- src/app/components/shared/ripple/ripple.directive.ts
+++ src/app/components/shared/ripple/ripple.directive.ts
@@ -5,45 +5,93 @@ import { Directive, ElementRef, HostListener } from '@angular/core';
 })
 export class RippleDirective {
 
-  @HostListener('mousedown', ['$event']) mouseDown(event){
+
+  ripple:any;
+  prevent:any;
+
+  @HostListener('mousedown', ['$event']) mouseDown(event){//DISPARA O RIPPLE AO CLIQUE
     let target = event.target;
 
-    let prevent = document.createElement('div');
-    prevent.classList.add('prevent');
+    this.prevent = document.createElement('div');
+    this.prevent.classList.add('prevent');
 
-    let ripple = document.createElement('div');
-    ripple.classList.add('ripple');
+    this.ripple = document.createElement('div');
+    this.ripple.classList.add('ripple');
 
     if(target.classList.contains('ripple') || target.classList.contains('prevent')){
-      target.parentNode.appendChild(prevent);
-      target.parentNode.appendChild(ripple);
+      target.parentNode.appendChild(this.prevent);
+      target.parentNode.appendChild(this.ripple);
     } else {
-      target.appendChild(prevent);
-      target.appendChild(ripple);
+      target.appendChild(this.prevent);
+      target.appendChild(this.ripple);
     }
-    ripple.style.top = (event.layerY)+"px";
+    this.ripple.style.top = (event.layerY)+"px";
     
-    ripple.style.left = (event.layerX)+"px";
+    this.ripple.style.left = (event.layerX)+"px";
 
     if(target.clientWidth >= target.clientHeight){	
 
-      ripple.style.transform = 'scale('+(this.elementRef.nativeElement.clientWidth*2.8)/12+')';	
+      this.ripple.style.transform = 'scale('+(this.elementRef.nativeElement.clientWidth*2.8)/12+')';	
 
     } else {
 
-      ripple.style.transform = 'scale('+(this.elementRef.nativeElement.clientHeight*2.8)/12+')';
+      this.ripple.style.transform = 'scale('+(this.elementRef.nativeElement.clientHeight*2.8)/12+')';
 
     }
 
-    setTimeout(()=>ripple.style.backgroundColor = 'transparent', 400);//RESET
-    setTimeout(()=>{
-      ripple.remove();
-      prevent.remove();
-    }, 800);
+  }
+
+  @HostListener('mouseup',['$event']) mouseUp(event){//REMOVE O RIPPLE AO MOUSE UP
+
+    if(event.target.classList.contains('prevent')){
+
+      setTimeout(() => {
+
+        try{
+          event.target.nextSibling.style.backgroundColor = 'transparent';
+        } catch{/*o ripple ja foi removido*/}
+
+      }, 200);//RESET
+
+      setTimeout(()=>{
+
+        try{
+          event.target.nextSibling.remove();
+          event.target.remove();
+        } catch{/*o ripple ja foi removido*/}
+      }, 600);
+
+    }
+
+  }
+
+  @HostListener('mouseout',['$event']) mouseOut(event){//REMOVE O RIPPLE AO MOUSE OUT
+    if(event.fromElement.classList.contains('prevent')){
+
+      setTimeout(() => {
+
+          try{
+            event.fromElement.nextSibling.style.backgroundColor = 'transparent';
+          } catch{/*o ripple ja foi removido*/}
+
+        },200);//RESET
+
+        setTimeout(()=>{
+
+          try {
+            event.target.nextSibling.remove();
+            event.target.remove();
+          } catch{/*o ripple ja foi removido*/}
+
+        }, 600);
+
+    }
   }
 
   constructor(
-    private elementRef: ElementRef
-  ) { }
+    private elementRef: ElementRef,
+  ) { 
+    this.elementRef.nativeElement.classList.add('ripple-container');
+  }
 
 }
